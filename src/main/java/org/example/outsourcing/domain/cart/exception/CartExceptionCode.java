c2a9124ba0264a04799e0f82fe0650f7a70c02b2
thezz9 <thezz9@naver.com>
2025-04-24 21:01:58 +0900

    fix: 주문 API 구현 수정

--- src/main/java/org/example/outsourcing/domain/cart/exception/CartExceptionCode.java
+++ src/main/java/org/example/outsourcing/domain/cart/exception/CartExceptionCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum CartExceptionCode implements ResponseCode {
 
     CART_ITEM_NOT_FOUND(false, HttpStatus.NOT_FOUND, "카트에 해당 상품이 존재하지 않습니다."),
-    INVALID_PRICE(false, HttpStatus.BAD_REQUEST, "가격이 일치하지 않습니다.");
+    INVALID_PRICE(false, HttpStatus.BAD_REQUEST, "가격이 일치하지 않습니다."),
+    CART_EMPTY(false, HttpStatus.BAD_REQUEST, "장바구니가 비어 있습니다.");
 
     private final boolean isSuccess;
     private final HttpStatus status;
