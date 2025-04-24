9a5a1feeb377624c970fc13fd1c65c02950b1fed
chat26666 <chat26666@gmail.com>
2025-04-24 15:54:13 +0900

    [feat] 구글 로그인 기능 구현

--- src/main/java/org/example/outsourcing/common/config/SecurityConfig.java
+++ src/main/java/org/example/outsourcing/common/config/SecurityConfig.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.example.outsourcing.common.filter.AccessJwtFilter;
 import org.example.outsourcing.common.filter.ExceptionJwtFilter;
 import org.example.outsourcing.common.filter.RefreshJwtFilter;
+import org.example.outsourcing.common.filter.handler.OauthSuccessHandler;
 import org.example.outsourcing.jwt.service.JwtService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,11 +31,13 @@ public class SecurityConfig {
 	private final RefreshJwtFilter refreshJwtFilter;
 	private final AccessJwtFilter accessJwtFilter;
 	private final ExceptionJwtFilter exceptionJwtFilter;
+	private final OauthSuccessHandler successHandler;
 
-	public SecurityConfig(JwtService jwtService) {
+	public SecurityConfig(JwtService jwtService, OauthSuccessHandler successHandler) {
 		this.refreshJwtFilter = new RefreshJwtFilter(jwtService);
 		this.accessJwtFilter = new AccessJwtFilter(jwtService);
 		this.exceptionJwtFilter = new ExceptionJwtFilter();
+		this.successHandler = successHandler;
 	}
 
 	@Bean
@@ -42,6 +46,27 @@ public class SecurityConfig {
 	}
 
 	@Bean
+	@Order(1)
+	public SecurityFilterChain oauth2Chain(HttpSecurity http) throws Exception {
+		http
+			.securityMatcher("/api/auth/oauth2/**")
+			.authorizeHttpRequests(a -> a
+				.anyRequest().authenticated()
+			)
+			.oauth2Login(oauth2 -> oauth2
+				.authorizationEndpoint(ep ->
+					ep.baseUri("/api/auth/oauth2/signin")
+				)
+				.redirectionEndpoint(re ->
+					re.baseUri("/api/auth/oauth2/callback")
+				)
+				.successHandler(successHandler)
+			);
+		return http.build();
+	}
+
+	@Bean
+	@Order(2)
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http
 			.cors(cors -> cors.configurationSource(corsConfigurationSource()))
@@ -58,9 +83,6 @@ public class SecurityConfig {
 					"/v2/**",
 					"/v3/**",
 					"/webjars/**"
-				).permitAll()
-				.requestMatchers(HttpMethod.GET,
-					"/api/auth/oauth2/signin/google"
 				).permitAll()
 				.anyRequest().authenticated()
 			)
