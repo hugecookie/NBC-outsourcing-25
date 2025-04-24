fc092e09f8b2cb7538d80cfd4db5b1c2ee1d35ba
thezz9 <thezz9@naver.com>
2025-04-24 20:05:41 +0900

    feat: #36 주문 API 구현

--- src/main/java/org/example/outsourcing/domain/order/repository/OrderRepository.java
+++ src/main/java/org/example/outsourcing/domain/order/repository/OrderRepository.java
@@ -1,7 +1,16 @@
 package org.example.outsourcing.domain.order.repository;
 
 import org.example.outsourcing.domain.order.entity.Order;
+import org.example.outsourcing.domain.store.entity.Store;
+import org.example.outsourcing.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findAllByUser(User user);
+
+    List<Order> findAllByStore(Store store);
+
 }
---
+++ src/main/java/org/example/outsourcing/domain/order/service/OrderService.java
@@ -0,0 +1,137 @@
+package org.example.outsourcing.domain.order.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.example.outsourcing.domain.cart.entity.Cart;
+import org.example.outsourcing.domain.cart.exception.CartException;
+import org.example.outsourcing.domain.cart.exception.CartExceptionCode;
+import org.example.outsourcing.domain.cart.respository.CartRepository;
+import org.example.outsourcing.domain.order.dto.reponse.OrderItemResponse;
+import org.example.outsourcing.domain.order.dto.reponse.OrderListResponse;
+import org.example.outsourcing.domain.order.dto.reponse.OrderResponse;
+import org.example.outsourcing.domain.order.dto.request.OrderSaveRequest;
+import org.example.outsourcing.domain.order.dto.request.OrderUpdateRequest;
+import org.example.outsourcing.domain.order.entity.Order;
+import org.example.outsourcing.domain.order.entity.OrderStatus;
+import org.example.outsourcing.domain.order.exception.OrderException;
+import org.example.outsourcing.domain.order.exception.OrderExceptionCode;
+import org.example.outsourcing.domain.order.repository.OrderRepository;
+import org.example.outsourcing.domain.store.entity.Store;
+import org.example.outsourcing.domain.store.exception.StoreException;
+import org.example.outsourcing.domain.store.exception.StoreExceptionCode;
+import org.example.outsourcing.domain.store.repository.StoreRepository;
+import org.example.outsourcing.domain.user.entity.User;
+import org.example.outsourcing.domain.user.exception.UserException;
+import org.example.outsourcing.domain.user.exception.UserExceptionCode;
+import org.example.outsourcing.domain.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderItemService orderItemService;
+
+    private final OrderRepository orderRepository;
+    private final UserRepository userRepository;
+    private final CartRepository cartRepository;
+    private final StoreRepository storeRepository;
+
+    public OrderResponse createOrder(Long userId, OrderSaveRequest request) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserException(UserExceptionCode.USER_NOT_FOUND));
+
+        List<Cart> carts = cartRepository.findAllByUser(user);
+
+        if (carts.isEmpty()) {
+            throw new CartException(CartExceptionCode.CART_EMPTY);
+        }
+
+        Store store = carts.get(0).getStore();
+        Integer totalPrice = orderItemService.getTotalPrice(carts);
+
+        if (store.getMinPrice() > totalPrice) {
+            throw new OrderException(OrderExceptionCode.UNDER_MINIMUM_ORDER_AMOUNT);
+        }
+
+        Order order = orderRepository.save(
+                Order.builder()
+                        .status(OrderStatus.ACCEPTED)
+                        .totalPrice(totalPrice)
+                        .phoneNumber(request.phoneNumber())
+                        .deliveryAddress(request.deliveryAddress())
+                        .user(user)
+                        .store(store)
+                        .build()
+        );
+
+        List<OrderItemResponse> orderItems = orderItemService.createOrderItems(order, carts);
+
+        return new OrderResponse(order, orderItems);
+    }
+
+    @Transactional
+    public List<OrderListResponse> getOrders(Long userId) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserException(UserExceptionCode.USER_NOT_FOUND));
+
+        List<Order> orders = orderRepository.findAllByUser(user);
+
+        return orderItemService.getOrderItemList(orders);
+    }
+
+    public OrderResponse getOrder(Long orderId) {
+
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new OrderException(OrderExceptionCode.ORDER_NOT_FOUND));
+
+        List<OrderItemResponse> orderItems = orderItemService.getOrderItem(orderId);
+        
+        return new OrderResponse(order, orderItems);
+    }
+
+    @Transactional
+    public List<OrderListResponse> getStoreOrders(Long storeId) {
+
+        Store store = storeRepository.findById(storeId)
+                .orElseThrow(() -> new StoreException(StoreExceptionCode.STORE_NOT_FOUND));
+
+        List<Order> orders = orderRepository.findAllByStore(store);
+
+        return orderItemService.getOrderItemList(orders);
+    }
+
+    @Transactional
+    public OrderResponse updateOrderStatus(Long orderId, OrderUpdateRequest request) {
+
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new OrderException(OrderExceptionCode.ORDER_NOT_FOUND));
+
+        if (order.getStatus().equals(OrderStatus.CANCELED)) {
+            throw new OrderException(OrderExceptionCode.INVALID_ORDER_STATUS);
+        }
+
+        List<OrderItemResponse> orderItems = orderItemService.getOrderItem(orderId);
+
+        order.updateOrder(request.orderStatus());
+
+        return new OrderResponse(order, orderItems);
+    }
+
+    @Transactional
+    public void canceledOrder(Long orderId) {
+
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new OrderException(OrderExceptionCode.ORDER_NOT_FOUND));
+
+        if (order.getStatus() != OrderStatus.ACCEPTED) {
+            throw new OrderException(OrderExceptionCode.COOKING_ALREADY_STARTED);
+        }
+
+        order.canceledOrder(OrderStatus.CANCELED);
+    }
+}
