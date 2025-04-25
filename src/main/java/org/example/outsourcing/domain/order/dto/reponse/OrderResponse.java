cda7b273f05181b573273be22786e2085e8af987
thezz9 <thezz9@naver.com>
2025-04-25 10:36:49 +0900

    refactor: #42 valid 추가, 생성자 추가

--- src/main/java/org/example/outsourcing/domain/order/dto/reponse/OrderResponse.java
+++ src/main/java/org/example/outsourcing/domain/order/dto/reponse/OrderResponse.java
@@ -2,17 +2,44 @@ package org.example.outsourcing.domain.order.dto.reponse;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
+import lombok.Getter;
 import org.example.outsourcing.domain.order.entity.Order;
+import org.example.outsourcing.domain.order.entity.OrderStatus;
 
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@Builder
-public record OrderResponse (
+@Getter
+public class OrderResponse {
 
-        Order order,
+    private final Long orderId;
 
-        List<OrderItemResponse> orderItems
+    private final Long storeId;
 
-) {
+    private final String storeName;
+
+    private final String userName;
+
+    private final String phoneNumber;
+
+    private final String deliveryAddress;
+
+    private final Integer totalPrice;
+
+    private final String orderStatus;
+
+    private final List<OrderItemResponse> orderItems;
+
+    public OrderResponse(Order order, List<OrderItemResponse> orderItems) {
+        this.orderId = order.getId();
+        this.storeId = order.getStore().getId();
+        this.storeName = order.getStore().getName();
+        this.userName = order.getUser().getName();
+        this.phoneNumber = order.getPhoneNumber();
+        this.deliveryAddress = order.getDeliveryAddress();
+        this.totalPrice = order.getTotalPrice();
+        this.orderStatus = order.getStatus().getDescription();
+        this.orderItems = orderItems;
+    }
 }
+
