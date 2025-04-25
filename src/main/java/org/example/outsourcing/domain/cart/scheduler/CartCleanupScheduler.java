27d15733f037507b5cdece9d1c6337b891cecece
thezz9 <thezz9@naver.com>
2025-04-25 19:22:34 +0900

    feat: #48 장바구니 TTL 구현

---
+++ src/main/java/org/example/outsourcing/domain/cart/scheduler/CartCleanupScheduler.java
@@ -0,0 +1,21 @@
+package org.example.outsourcing.domain.cart.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import org.example.outsourcing.domain.cart.respository.CartRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class CartCleanupScheduler {
+
+    private final CartRepository cartRepository;
+
+    @Scheduled(fixedRate = 60 * 60 * 1000)
+    public void deleteOldCarts() {
+        LocalDateTime oneHourAgo = LocalDateTime.now().minusHours(1);
+        cartRepository.deleteByUpdatedAtBefore(oneHourAgo);
+    }
+}
