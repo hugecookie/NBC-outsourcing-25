eaeb2cb84377158fac92b5d2ed493653d7fd2e6f
hugecookie <phrpp5770@gmail.com>
2025-04-25 18:24:46 +0900

    feat: 메뉴 이미지 변경 시 권한 예외 및 메뉴 없음 예외 코드 추가

--- src/main/java/org/example/outsourcing/domain/menu/exception/MenuExceptionCode.java
+++ src/main/java/org/example/outsourcing/domain/menu/exception/MenuExceptionCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum MenuExceptionCode implements ResponseCode {
 
-    MENU_NOT_FOUND(false, HttpStatus.NOT_FOUND, "메뉴를 찾을 수 없습니다.");
+    MENU_NOT_FOUND(false, HttpStatus.NOT_FOUND, "메뉴를 찾을 수 없습니다."),
+    MENU_FORBIDDEN(false, HttpStatus.FORBIDDEN, "해당 메뉴에 대한 권한이 없습니다.");
 
     private final boolean isSuccess;
     private final HttpStatus status;
