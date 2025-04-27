c576294a28e551e75a372a3bc95b8ed3e32a550d
thezz9 <thezz9@naver.com>
2025-04-27 23:55:59 +0900

    fix: #61 N+1 문제 해결

--- src/main/java/org/example/outsourcing/domain/order/repository/OrderItemRepository.java
+++ src/main/java/org/example/outsourcing/domain/order/repository/OrderItemRepository.java
@@ -2,14 +2,17 @@ package org.example.outsourcing.domain.order.repository;
 
 import org.example.outsourcing.domain.order.entity.Order;
 import org.example.outsourcing.domain.order.entity.OrderItem;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
-    List<OrderItem> findAllById(Long orderId);
+    @EntityGraph(attributePaths = {"menu"})
+    List<OrderItem> findAllByOrderId(Long orderId);
 
+    @EntityGraph(attributePaths = {"menu"})
     List<OrderItem> findAllByOrderIn(List<Order> orders);
 
 }
--- src/main/java/org/example/outsourcing/domain/order/repository/OrderRepository.java
+++ src/main/java/org/example/outsourcing/domain/order/repository/OrderRepository.java
@@ -3,14 +3,17 @@ package org.example.outsourcing.domain.order.repository;
 import org.example.outsourcing.domain.order.entity.Order;
 import org.example.outsourcing.domain.store.entity.Store;
 import org.example.outsourcing.domain.user.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+    @EntityGraph(attributePaths = {"user", "store"})
     List<Order> findAllByUser(User user);
 
+    @EntityGraph(attributePaths = {"user", "store"})
     List<Order> findAllByStore(Store store);
 
 }
