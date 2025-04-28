a1452524c03f1c18de9bd8df017f79634de1f7ff
yeeun0702 <kimyeeun0702@naver.com>
2025-04-28 10:53:17 +0900

    [refactor] #63 알림 접근 권한 관련 예외 추가

--- src/main/java/org/example/outsourcing/domain/notification/exception/NotificationExceptionCode.java
+++ src/main/java/org/example/outsourcing/domain/notification/exception/NotificationExceptionCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum NotificationExceptionCode implements ResponseCode {
     NOT_FOUND_FCMTOKEN(false, HttpStatus.NOT_FOUND, "FCM 토큰을 찾을 수 없습니다."),
-    NOT_FOUND_NOTIFICATION(false, HttpStatus.NOT_FOUND, "알림을 찾을 수 없습니다.");
+    NOT_FOUND_NOTIFICATION(false, HttpStatus.NOT_FOUND, "알림을 찾을 수 없습니다."),
+    NO_AUTH_FOR_NOTIFICATION(false, HttpStatus.FORBIDDEN, "알림에 대한 접근 권한이 없습니다.");
 
     private final boolean isSuccess;
     private final HttpStatus status;
