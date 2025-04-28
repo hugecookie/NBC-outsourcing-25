95922b2048e297e81405030dad1fe6f9894f53b4
chat26666 <chat26666@gmail.com>
2025-04-28 16:56:40 +0900

    [comment] 유저, 인증 서비스 레이어 주석 추가

--- src/main/java/org/example/outsourcing/domain/user/service/UserService.java
+++ src/main/java/org/example/outsourcing/domain/user/service/UserService.java
@@ -35,6 +35,12 @@ public class UserService {
 	private final ApplicationEventPublisher publisher;
 	private final S3Service s3Service;
 
+	/**
+	 * 회원 가입<br>
+	 * 유저 정보를 입력 받은 후 이메일 중복검증 후 저장 진행
+	 * @param request 이메일, 패스워드, 이름, 역할(admin, user, owner)
+	 * @author 박경오
+	 */
 	@Transactional
 	public void createUser(UserSaveRequest request) {
 
@@ -56,6 +62,16 @@ public class UserService {
 		);
 	}
 
+	/**
+	 * 회원 탈퇴<br>
+	 * 이메일, 비밀번호를 검증받고 회원 탈퇴 진행<br>
+	 * Soft Delete 방식<br>
+	 * 탈퇴 후, 해당 계정의 토큰은 블랙리스트 처리
+	 * @param request 검증에 필요한 이메일, 비밀번호
+	 * @param userId 토큰에 저장된 userId
+	 * @param accessToken 액세스 토큰
+	 * @author 박경오
+	 */
 	@Transactional
 	@PreAuthorize("@userz.checkUserId(authentication.principal.id, #request.email())")
 	public void withdrawUser(UserDeleteRequest request, Long userId, String accessToken) {
@@ -71,6 +87,13 @@ public class UserService {
 		publisher.publishEvent(new UserWithDrawEvent(accessToken));
 	}
 
+	/**
+	 * 회원 수정<br>
+	 * 이메일, 비밀번호를 검증받고 회원 수정 진행<br>
+	 * 소셜 가입 유저는 수정불가<br>
+	 * @param request 검증에 필요한 이메일, 비밀번호, 신규 이름, 신규 패스워드
+	 * @author 박경오
+	 */
 	@Transactional
 	@PreAuthorize("hasAnyRole('owner', 'admin', 'user') and @userz.checkUserId(authentication.principal.id, #request.email())")
 	public void modifyUser(UserModifyRequest request) {
@@ -88,6 +111,13 @@ public class UserService {
 		);
 	}
 
+	/**
+	 * 회원 조회<br>
+	 * 현재 접속중인 계정의 정보 조회
+	 * @param userAuth 조회에 필요한 계정 ID
+	 * @return 이름, 생성일자, 프로필 이미지 주소, 사용중인 플랫폼
+	 * @author 박경오
+	 */
 	@Transactional(readOnly = true)
 	public UserResponse viewUser(UserAuth userAuth) {
 		User user = userRepository.findById(userAuth.getId())
